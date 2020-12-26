ac572701374d189167e668a376e84db11d1ef236
Muhammad Azhar Hussain <44233088+Azhar7909@users.noreply.github.com>
2020-12-26 23:44:31 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [repose, setRepose] = useState([{}]);
+  // const [data, setData] = useState({})
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts/1")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setData(json)
+  //       console.log(json);
+  //     });
+  // }, []);
+
+  useEffect( () => {
+    async function getRepos(params) {
+      const response = await fetch(
+        "https://api.github.com/users/Azhar7909/repos"
+      );
+      const data = await response.json();
+      console.log(data);
+      setRepose(data);
+    }
+    getRepos()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Async Await in React</h1>
+      {/* <h2>{data.title}</h2> */}
+      <ul>
+        {repose.map((rep, ind) => {
+          return <li key={ind}>{rep.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
